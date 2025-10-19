bc469f73276a5339cf19fe761c677083f2fbcd4d
MaicolCode <paucarjhon07@outlook.com>
2025-10-18 22:14:47 -0500

    Datos de los videos de trading de la comunidad

---
+++ src/data/urlYT.mock.ts
@@ -0,0 +1,15 @@
+import type { YTUrl } from "../types";
+
+export const tradingNovatos: Array<YTUrl> = [
+    {
+        title: 'Trading para Novatos - Sesion 1',
+        url: 'https://www.youtube.com/embed/VcX1bL4lyk0'
+    },{
+        title: 'Trading para Novatos - Sesion 2',
+        url: 'https://www.youtube.com/embed/k2MSeeZbEaQ'
+    },{
+        title: 'Trading para Novatos - Sesion 3',
+        url: 'https://www.youtube.com/embed/6ZimHCukVzI'
+    }
+]
+
