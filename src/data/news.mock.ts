371692258301538b6689b098ef88f94ae6a68bf0
MaicolCode <paucarjhon07@outlook.com>
2025-10-18 21:55:04 -0500

    Mock de datos que contiene la lista de noticias

---
+++ src/data/news.mock.ts
@@ -0,0 +1,82 @@
+import type { News } from "../types"
+
+
+export const technicalNews: Array<News> = [
+  {
+    description:
+      'La incertidumbre se a,podera del oro, bitcoin, dólar, crudo e índices',
+    date: '08-07-2025',
+    url: 'https://es.investing.com/analysis/la-incertidumbre-se-apodera-del-oro-bitcoin-dolar-crudo-e-indices-200478553'
+  },
+  {
+    description: 'Cómo convertir cada decisión en una lección de éxito',
+    date: '08-07-2025',
+    url: 'https://es.investing.com/analysis/como-convertir-cada-decision-en-una-leccion-de-exito-200478011'
+  },
+  {
+    description:
+      'Este error destruye carteras (y Buffett lo evita con una simple regla...)',
+    date: '08-05-2025',
+    url: 'https://es.investing.com/analysis/este-error-destruye-carteras-y-buffett-lo-evita-con-una-simple-regla-200477963'
+  },
+  {
+    description: 'Los 4 Pasos que transformarán tu forma de decidir',
+    date: '07-31-2025',
+    url: 'https://es.investing.com/analysis/los-4-pasos-que-transformaran-tu-forma-de-decidir-200477889'
+  },
+  {
+    description: '5 Estrategias mentales para blindarse en bolsa',
+    date: '07-29-2025',
+    url: 'https://es.investing.com/analysis/5-estrategias-mentales-para-blindarse-en-bolsa-200477852'
+  },
+  {
+    description: 'Mira esto en 4 min y superarás al 90% de los traders',
+    date: '04-08-2025',
+    url: 'https://es.investing.com/analysis/mira-esto-en-4-min-y-superaras-al-90-de-los-traders-200477935'
+  },
+  {
+    description: 'Análisis y situación del oro, bitcoin, dólar, crudo e índices ',
+    date: '06-10-2025',
+    url: 'https://es.investing.com/analysis/analisis-y-situacion-del-oro-bitcoin-dolar-crudo-e-indices-200479026'
+  }
+]
+
+export const investmentNews: Array<News> = [
+  {
+    description:
+      'Estudiar trading como loco no te hace rentable (y sí, lo estás haciendo mal)',
+    date: '08-06-2025',
+    url: 'https://es.investing.com/analysis/estudiar-trading-como-loco-no-te-hace-rentable-y-si-lo-estas-haciendo-mal-200478044'
+  },
+  {
+    description: 'Mira esto en 4 min y superarás al 90% de los traders',
+    date: '08-04-2025',
+    url: 'https://es.investing.com/analysis/mira-esto-en-4-min-y-superaras-al-90-de-los-traders-200477935'
+  },
+  {
+    description: 'Atento al mes de agosto que podría haber fuertes curvas',
+    date: '08-04-2025',
+    url: 'https://es.investing.com/analysis/atento-al-mes-de-agosto-que-puede-haber-fuertes-curvas-200477822'
+  },
+  {
+    description: 'Las 5 reglas para invertir mejor',
+    date: '07-28-2025',
+    url: 'https://es.investing.com/analysis/las-5-reglas-para-invertir-mejor-200477609'
+  },
+  {
+    description:
+      'De 16 horas diarias a 3: cómo el trading me devolvió la vida (y la cabeza)',
+    date: '07-30-2025',
+    url: 'https://es.investing.com/analysis/de-16-horas-diarias-a-3-como-el-trading-me-devolvio-la-vida-y-la-cabeza-200477878'
+  },
+  {
+    description: 'Acciones clave para esta semana en España, México y EE.UU.',
+    date: '08-06-2025',
+    url: 'https://es.investing.com/analysis/acciones-clave-para-esta-semana-en-espana-mexico-y-eeuu-200477998'
+  },
+  {
+    description: 'Asesorar sobre lo que la élite está comprando: oro físico',
+    date: '07-30-2025',
+    url: 'https://es.investing.com/analysis/asesorar-sobre-lo-que-la-elite-esta-comprando-oro-fisico-200477869'
+  }
+]
